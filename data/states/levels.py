b0b2938425ea0a39e2f197097e9b929e72808ac3
KounRyuSui <yarkovish@gmail.com>
2015-04-28 22:07:36 -0400

    Added and inserted more music

--- data/states/levels.py
+++ data/states/levels.py
@@ -72,11 +72,11 @@ class LevelState(tools._State):
         """
         Set music based on name.
         """
-        music_dict = {c.FAESLANDING: ('town_theme', .4),
-                      c.OVERWORLD: ('plateau', .4),
-                      c.SOUTHFIELD: ('plateau', .4),
-                      c.WESTFIELD: ('plateau', .4),
-                      c.CASTLE: ('town_theme', .4),
+        music_dict = {c.FAESLANDING: ('faes_landing', .8),
+                      c.OVERWORLD: ('nikko', .8),
+                      c.SOUTHFIELD: ('nikko', .8),
+                      c.WESTFIELD: ('nikko', .8),
+                      c.CASTLE: ('faes_landing', .8),
                       c.DUNGEON: ('dungeon_theme', .4),
                       c.DUNGEON2: ('dungeon_theme', .4),
                       c.DUNGEON3: ('dungeon_theme', .4),
